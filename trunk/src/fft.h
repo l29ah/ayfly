d305b93429999df9a0fc0a5084ae0620e248e17d
andrew@it-optima.ru <andrew@it-optima.ru@0fa6c16a-c44e-0410-b361-b1c6067f254c>
2008-07-22 06:47:54 +0000

    Added credits

--- trunk/src/fft.h
+++ trunk/src/fft.h
@@ -1,6 +1,6 @@
 /***************************************************************************
- *   Copyright (C) 2008 by Deryabin Andrew   							   *
- *   andrewderyabin@gmail.com   										   *
+ *   Copyright (C) 2008 by Deryabin Andrew   				               *
+ *   andrew@it-optima.ru                                                   *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
