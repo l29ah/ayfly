d08c886ee127de99de6faf95fd283f7d716b821d
andrew <andrew@it-optima.ru>
2008-07-22 06:47:54 +0000

    Added credits

--- src/s60/sis/build.sh
+++ src/s60/sis/build.sh
@@ -4,4 +4,4 @@ abld build gcce urel
 cd ../sis
 makesis ayfly_s60_S60_3_X_v_1_0_0.pkg
 signsis ayfly_s60_S60_3_X_v_1_0_0.sis ayfly_s60_S60_3_X_v_1_0_0.sisx gencert.cert genkey.key
-obexftp -b 00:1E:3A:2D:98:F9 -c "E:\Others\ayfly" -p ayfly_s60_S60_3_X_v_1_0_0.sisx
+
