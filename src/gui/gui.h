cdf86f452c34214a6024d95417b89ac7adc51c3b
andrew <andrew@it-optima.ru>
2008-08-07 20:41:40 +0000

    Reorganizing source tree

---
+++ src/gui/gui.h
@@ -0,0 +1,81 @@
+/***************************************************************************
+ *   Copyright (C) 2008 by Deryabin Andrew                                 *
+ *   andrew@it-optima.ru                                                   *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+#ifndef COMMON_H_
+#    define COMMON_H_
+
+#    ifndef UNICODE
+#        define UNICODE
+#    endif
+
+#    ifndef _UNICODE
+#        define _UNICODE
+#    endif
+
+#    ifdef _WINDOWS
+#       ifndef WINDOWS
+#           define WINDOWS
+#       endif
+#    endif
+
+#    ifdef WINDOWS
+#       ifndef _WINDOWS
+#           define _WINDOWS
+#       endif
+#        define WIN32_LEAN_AND_MEAN
+#    endif
+
+
+#    ifdef _MSC_VER
+#        pragma warning(disable:4309)
+#    endif
+
+#include "ayfly.h"
+
+struct bindings
+{
+    wxString name; /* button name */
+    wxString str_id; /* string id for action */
+    int id; /* int id for action */
+    int key; /* key mapping */
+    int modifier; /* modifier */
+};
+
+#        include "wx/wx.h"
+#        include "wx/file.h"
+#        include "wx/filedlg.h"
+#        include "wx/toolbar.h"
+#        include "wx/dnd.h"
+#        include "wx/slider.h"
+#        include "wx/spinbutt.h"
+#        include "wx/tglbtn.h"
+#        include "wx/listctrl.h"
+#        include "wx/dialog.h"
+
+#ifndef sizeof_array
+#    define sizeof_array(x) sizeof(x) / sizeof(x [0])
+#endif
+
+#        include "AyflyApp.h"
+#        include "AyflyFrame.h"
+#        include "DnDFiles.h"
+#        include "AyflyBindingsDlg.h"
+
+#endif /*COMMON_H_*/
---
+++ src/s60/inc/s60.h
@@ -0,0 +1,61 @@
+/***************************************************************************
+ *   Copyright (C) 2008 by Deryabin Andrew                                 *
+ *   andrew@it-optima.ru                                                   *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+#ifndef COMMON_H_
+#    define COMMON_H_
+
+#    ifndef UNICODE
+#        define UNICODE
+#    endif
+
+#    ifndef _UNICODE
+#        define _UNICODE
+#    endif
+
+#ifndef sizeof_array
+#    define sizeof_array(x) sizeof(x) / sizeof(x [0])
+#endif
+
+#include "ayfly.h"
+
+#include "eikstart.h"
+#include "f32file.h"
+#include "s32std.h"
+#include "s32file.h"
+#include "e32cmn.h"
+#include "avkon.hrh"
+#include "coemain.h"
+#include "aknmessagequerydialog.h"
+#include "aknnotewrappers.h"
+#include "stringloader.h"
+#include "hlplch.h"
+#include "caknfileselectiondialog.h"
+#include "pathinfo.h"
+
+#include "ayfly_s60.hrh"
+#include "ayfly_s60.pan"
+#include "ayfly_s60Document.h"
+#include "ayfly_s60Application.h"
+#include "ayfly_s60AppUi.h"
+#include "ayfly_s60AppView.h"
+#include "ayfly_s60Audio.h"
+#include "ayfly_s60_0xEA0B66F6.rsg"
+
+#endif /*COMMON_H_*/
