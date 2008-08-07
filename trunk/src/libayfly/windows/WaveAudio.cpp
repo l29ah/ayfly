a958c0051823f89e0e64535759fa35b1250c45d0
andrew@it-optima.ru <andrew@it-optima.ru@0fa6c16a-c44e-0410-b361-b1c6067f254c>
2008-08-07 20:46:30 +0000

    Reorganizing source tree

--- trunk/src/libayfly/windows/WaveAudio.cpp
+++ trunk/src/libayfly/windows/WaveAudio.cpp
@@ -18,7 +18,7 @@
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
 
-#include "common.h"
+#include "ayfly.h"
 
 WaveAudio::WaveAudio(unsigned long _sr)
 :AbstractAudio(_sr)
