7c5a19f058bacbe96eb08c15d03175aa7a36bb73
andrew <andrew@it-optima.ru>
2008-08-07 20:46:30 +0000

    Reorganizing source tree

--- src/libayfly/unix/SDLAudio.cpp
+++ src/libayfly/unix/SDLAudio.cpp
@@ -18,7 +18,7 @@
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
 
-#include "common.h"
+#include "ayfly.h"
 
 SDLAudio::SDLAudio(unsigned long _sr) :
     AbstractAudio(_sr)
