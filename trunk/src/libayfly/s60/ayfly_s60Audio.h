c5e74975477b1db1212b6f8ac64ecea17e1a8b16
andrewderyabin <andrewderyabin@0fa6c16a-c44e-0410-b361-b1c6067f254c>
2008-08-21 12:55:42 +0000

    Working on s60 build

--- trunk/src/libayfly/s60/ayfly_s60Audio.h
+++ trunk/src/libayfly/s60/ayfly_s60Audio.h
@@ -1,24 +1,69 @@
 #ifndef S60AUDIO_H_
 #define S60AUDIO_H_
 
-#include "mmf/server/sounddevice.h"
-#include "mmf/common/mmfutilities.h"
+#include <Mda\Common\Audio.h>
+#include <MdaAudioOutputStream.h>
+#include <e32std.h>
 
-class Cayfly_s60Audio : public AbstractAudio, MDevSoundObserver
+#define AYFLY_SERVER_STACKSIZE        65536
+
+#define AYFLY_COMMAND_NONE            0
+#define AYFLY_COMMAND_START_PLAYBACK  1
+#define AYFLY_COMMAND_STOP_PLAYBACK   2
+#define AYFLY_COMMAND_SET_VOLUME      3
+#define AYFLY_COMMAND_EXIT            4
+#define AYFLY_COMMAND_WAIT_KILL       5
+
+#define MIX_BUFFER_TIMES                                     8                // mix in 8 smaller passes
+#define MIX_BUFFER_SAMPLES_IN_ONE_STEP                     2048
+#define MIX_BUFFER_LENGTH               MIX_BUFFER_SAMPLES_IN_ONE_STEP*MIX_BUFFER_TIMES*4
+
+class Cayfly_s60Audio;
+
+class CCommandHandler: public CActive
 {
 public:
-    Cayfly_s60Audio(AYSongInfo *info);
+    static CCommandHandler* NewL();
+    ~CCommandHandler();
+    void Start(Cayfly_s60Audio *aSound);
+    void DoCancel();
+
+    CCommandHandler();
+    void RunL();
+private:
+    Cayfly_s60Audio *iSound;
+};
+
+class Cayfly_s60Audio: public AbstractAudio, CBase, MMdaAudioOutputStreamCallback
+{
+public:
+    enum
+    {
+        EStopped = 0, EStarting, EPlaying, EStopping
+    };
+public:
+    static Cayfly_s60Audio* NewL(AYSongInfo *info);
     virtual ~Cayfly_s60Audio();
 
     void StartPlay();
     void StopPlay();
-
     void SetDeviceVolume(TInt aVolume);
     TInt GetDeviceVolume();
 
     bool Start();
-	void Stop();
-
+    void Stop();
+    TPtr8 iDesc1;
+    TPtr8 iDesc2;
+    RThread iPlayerThread;
+    TInt iBufferToMix;
+    CIdle *iIdle;
+    TBool iIdleActive;
+    TBool iStartOnNext;
+    CMdaAudioOutputStream *iStream;
+    TMdaAudioDataSettings iSettings;
+    TInt iState;
+    TInt iVolume;
+    CCommandHandler *iHandler;
 private:
     void KillSound();
 
@@ -34,13 +79,9 @@ private:
 
     void DisplayError(const TDesC& aTitle, TInt aError);
 
-private:
-    TMMFPrioritySettings  iPrioritySettings;
-    CMMFDevSound* iDevSound;
+    void ConstructL();
+    Cayfly_s60Audio(AYSongInfo *info);
 
-    TFourCC   iCodecType;
-    TInt      iVolume;
-    TUint8* iSoundData; // sound buffer
 };
 
 #endif /*S60AUDIO_H_*/
