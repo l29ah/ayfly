6af315137da8605521b9094ef2166646f8804f42
andrewderyabin <andrewderyabin@0fa6c16a-c44e-0410-b361-b1c6067f254c>
2008-08-21 09:59:06 +0000

    Working on s60 build

--- src/libayfly/common.cpp
+++ src/libayfly/common.cpp
@@ -18,8 +18,7 @@
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
 
-//#include "ayfly.h"
-#include "s60.h"
+#include "ayfly.h"
 
 AYSongInfo *ay_sys_getnewinfo()
 {
