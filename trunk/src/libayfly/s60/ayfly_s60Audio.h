1f2bf77871c7a8d42e10fc6fcb3a10cd3a796cc0
andrew@it-optima.ru <andrew@it-optima.ru@0fa6c16a-c44e-0410-b361-b1c6067f254c>
2008-08-22 16:38:42 +0000

    Working on s60 build

--- trunk/src/libayfly/s60/ayfly_s60Audio.h
+++ trunk/src/libayfly/s60/ayfly_s60Audio.h
@@ -75,20 +75,6 @@ public:
     CCommandHandler *iHandler;
     bool stereo;
 private:
-    void KillSound();
-
-    // from MDevSoundObserver
-    void BufferToBeFilled(CMMFBuffer *aBuffer);
-    void InitializeComplete(TInt aError);
-    void ToneFinished(TInt aError);
-    void PlayError(TInt aError);
-    void BufferToBeEmptied(CMMFBuffer *aBuffer);
-    void RecordError(TInt aError);
-    void ConvertError(TInt aError);
-    void DeviceMessage(TUid aMessageType, const TDesC8 &aMsg);
-
-    void DisplayError(const TDesC& aTitle, TInt aError);
-
     void ConstructL();
     Cayfly_s60Audio(AYSongInfo *info);
     AYSongInfo *songinfo;
