dba70225999c09d84a5bc31831ee4ec300a71479
andrew <andrew@it-optima.ru>
2008-08-24 16:51:06 +0000

    Added PSG format support

--- src/libayfly/s60/ayfly_s60Audio.cpp
+++ src/libayfly/s60/ayfly_s60Audio.cpp
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright (C) 2008 by Deryabin Andrew                      *
+ *   Copyright (C) 2008 by Deryabin Andrew                                 *
  *   andrew@it-optima.ru                                                   *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
