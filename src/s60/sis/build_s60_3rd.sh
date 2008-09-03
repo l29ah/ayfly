38e716320bd4150c5f29ecf64d91f028dcf85217
andrew <andrew@it-optima.ru>
2008-09-03 10:46:22 +0000

    Working..

--- src/s60/sis/build_s60_3rd.sh
+++ src/s60/sis/build_s60_3rd.sh
@@ -4,4 +4,4 @@ bldmake bldfiles
 abld build gcce urel
 cd ../sis
 makesis ayfly_s60_3rd.pkg
-signsis ayfly_s60_3rd.sis ayfly_s60_3rd_0.0.16.sisx gencert.cert genkey.key
+signsis ayfly_s60_3rd.sis ayfly_s60_3rd_0.0.16.sisx gensign.cer gensign.key
