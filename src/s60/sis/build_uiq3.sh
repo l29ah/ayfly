99547f07def7dd812392379dece627f77ebd0199
andrew <andrew@it-optima.ru>
2008-09-06 19:32:59 +0000

    Version 0.0.17 ready

--- src/s60/sis/build_uiq3.sh
+++ src/s60/sis/build_uiq3.sh
@@ -4,4 +4,4 @@ bldmake bldfiles
 abld build gcce urel
 cd ../sis
 makesis ayfly_uiq3.pkg
-signsis ayfly_uiq3.sis ayfly_uiq3_0.0.16.sisx gensign.cer gensign.key
+signsis ayfly_uiq3.sis ayfly_uiq3_0.0.17.sisx gensign.cer gensign.key
