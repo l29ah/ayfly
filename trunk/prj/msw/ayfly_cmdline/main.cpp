ee478be6744216f9e537f92cd096103b29c4ca42
andrew@it-optima.ru <andrew@it-optima.ru@0fa6c16a-c44e-0410-b361-b1c6067f254c>
2008-09-08 12:04:04 +0000

    Visual Studio project changes

--- trunk/prj/msw/ayfly_cmdline/main.cpp
+++ trunk/prj/msw/ayfly_cmdline/main.cpp
@@ -1,4 +1,4 @@
-#include "ayfly_dynamicdll.h" //for usual dll linking use ayfly.h
+#include "ayfly.h" //for usual dll linking use ayfly.h
 #include <windows.h>
 #include <iostream>
 #include <stdio.h>
@@ -10,16 +10,6 @@ void song_end(void *songinfo);
 //simple end flag
 bool end;
 
-//imports that will be used.
-ay_initsong pay_initsong;
-ay_setcallback pay_setcallback;
-ay_startsong pay_startsong;
-ay_getelapsedtime pay_getelapsedtime;
-ay_stopsong pay_stopsong;
-ay_sethwnd pay_sethwnd;
-ay_songstarted pay_songstarted;
-ay_getsonglength pay_getsonglength;
-
 int _tmain(int argc, TCHAR **argv)
 {
     if(argc < 2)
@@ -27,56 +17,38 @@ int _tmain(int argc, TCHAR **argv)
         cout << "At least one filename must be given!" << endl;
         return 0;
     }
-    cout << "Loading ayfly.dll.." << endl;
-    HMODULE hDll = LoadLibraryW(L"ayfly.dll");
-    if(hDll == NULL)
-    {
-        cout << "Error loading ayfly.dll" << endl;
-        return 1;
-    }
-    cout << "Dll successfully loaded." << endl;
-
-    //get pointers to functions;
-    pay_initsong = (ay_initsong)GetProcAddress(hDll, "ay_initsong");
-    pay_setcallback = (ay_setcallback)GetProcAddress(hDll, "ay_setcallback");
-    pay_startsong = (ay_startsong)GetProcAddress(hDll, "ay_startsong");
-    pay_getelapsedtime = (ay_getelapsedtime)GetProcAddress(hDll, "ay_getelapsedtime");
-    pay_stopsong = (ay_stopsong)GetProcAddress(hDll, "ay_stopsong");
-    pay_sethwnd = (ay_sethwnd)GetProcAddress(hDll, "ay_sethwnd");
-    pay_songstarted = (ay_songstarted)GetProcAddress(hDll, "ay_songstarted");
-    pay_getsonglength = (ay_getsonglength)GetProcAddress(hDll, "ay_getsonglength");
+    
 
     for(int i = 1; i < argc; i++)
     {
         TCHAR *filename = argv [i];
         cout << "Loading song " << filename << ".." << endl;
-        void *songinfo = pay_initsong(filename, 44100);
+        void *songinfo = ay_initsong(filename, 44100);
         if(songinfo == 0)
         {
             cout << "Error loading song. Skipping.." << endl;
             continue;
         }
         cout << "Playing song " << filename << ".." << endl;
-        cout << "Song length = " << (pay_getsonglength(songinfo) / 50) << " seconds.." << endl;
+        cout << "Song length = " << (ay_getsonglength(songinfo) / 50) << " seconds.." << endl;
         end = false;
-        pay_setcallback(songinfo, song_end, songinfo);
+        ay_setcallback(songinfo, song_end, songinfo);
         //important!! our window handle must be set BEFORE playback start!
         HWND hWnd = GetConsoleWindow();
-        pay_sethwnd(songinfo, hWnd);
-        pay_startsong(songinfo);
-        bool ret = pay_songstarted(songinfo);
+        ay_sethwnd(songinfo, hWnd);
+        ay_startsong(songinfo);
+        bool ret = ay_songstarted(songinfo);
         while(!end)
         {
             Sleep(10);
-            unsigned long elapsed = pay_getelapsedtime(songinfo) / 50;
+            unsigned long elapsed = ay_getelapsedtime(songinfo) / 50;
             printf("Elapsed time: %lu seconds\r", elapsed);
         }        
-        pay_stopsong(songinfo);
+        ay_stopsong(songinfo);
         cout << endl;
         cout << "Song " << filename << " ended." << endl;
     }
     //close library handle
-    FreeLibrary(hDll);
     return 0;
 
 }
