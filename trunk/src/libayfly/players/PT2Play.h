07380913d790c5dfdc70befd6b0fdfeb5dd42888
andrew@it-optima.ru <andrew@it-optima.ru@0fa6c16a-c44e-0410-b361-b1c6067f254c>
2009-01-04 22:13:27 +0000

    rewritting PT2 player..

--- trunk/src/libayfly/players/PT2Play.h
+++ trunk/src/libayfly/players/PT2Play.h
@@ -14,15 +14,113 @@ struct PT2_File
     unsigned char PT2_PositionList[65535 - 131];
 };
 
+struct PT2_Channel_Parameters
+{
+    unsigned short Address_In_Pattern, OrnamentPointer, SamplePointer, Ton;
+    unsigned char Loop_Ornament_Position, Ornament_Length, Position_In_Ornament, Loop_Sample_Position, Sample_Length, Position_In_Sample, Volume, Number_Of_Notes_To_Skip, Note, Slide_To_Note, Amplitude;
+    signed char Current_Ton_Sliding, Ton_Delta;
+    int GlissType;
+    bool Envelope_Enabled, Enabled;
+    signed char Glissade, Addition_To_Noise, Note_Skip_Counter;
+};
+
+struct PT2_Parameters
+{
+    unsigned char DelayCounter, Delay, CurrentPosition;
+};
+
+struct PT2_SongInfo
+{
+    PT2_Parameters PT2;
+    PT2_Channel_Parameters PT2_A, PT2_B, PT2_C;
+};
+
+#define PT2_A ((PT2_SongInfo *)info.data)->PT2_A
+#define PT2_B ((PT2_SongInfo *)info.data)->PT2_B
+#define PT2_C ((PT2_SongInfo *)info.data)->PT2_C
+#define PT2 ((PT2_SongInfo *)info.data)->PT2
+
 #define PT2_SamplesPointers(x) (header->PT2_SamplesPointers0 [(x) * 2] | (header->PT2_SamplesPointers0 [(x) * 2 + 1] << 8))
 #define PT2_OrnamentsPointers(x) (header->PT2_OrnamentsPointers0 [(x) * 2] | (header->PT2_OrnamentsPointers0 [(x) * 2 + 1] << 8))
+#define PT2_PositionList(x) (header->PT2_PositionList [(x)])
 #define PT2_PatternsPointer (header->PT2_PatternsPointer0 | (header->PT2_PatternsPointer1 << 8))
+#define PT2_Delay (header->PT2_Delay)
 
 void PT2_Init(AYSongInfo &info)
 {
-    if(!ay_sys_initz80(info))
+    unsigned char *module = info.module;
+    PT2_File *header = (PT2_File *)module;
+    if(info.data)
+    {
+        delete (PT2_SongInfo *)info.data;
+        info.data = 0;
+    }
+    info.data = (void *)new PT2_SongInfo;
+    if(!info.data)
         return;
-    ay_sys_initz80module(info, 0xc000, PT2Play_data, sizeof(PT2Play_data), 0xc000, 0xc006);
+    memset(&PT2_A, 0, sizeof(PT2_Channel_Parameters));
+    memset(&PT2_B, 0, sizeof(PT2_Channel_Parameters));
+    memset(&PT2_C, 0, sizeof(PT2_Channel_Parameters));
+    PT2.DelayCounter = 1;
+    PT2.Delay = PT2_Delay;
+    PT2.CurrentPosition = 0;
+
+    PT2_A.Address_In_Pattern = ay_sys_getword(&module[PT2_PatternsPointer+ PT2_PositionList(0) * 6]);
+    PT2_B.Address_In_Pattern = ay_sys_getword(&module [PT2_PatternsPointer + PT2_PositionList(0) * 6 + 2]);
+    PT2_C.Address_In_Pattern = ay_sys_getword(&module [PT2_PatternsPointer + PT2_PositionList(0) * 6 + 4]);
+
+    PT2_A.OrnamentPointer = PT2_OrnamentsPointers(0);
+    PT2_A.Ornament_Length = module[PT2_A.OrnamentPointer];
+    PT2_A.OrnamentPointer++;
+    PT2_A.Loop_Ornament_Position = module[PT2_A.OrnamentPointer];
+    PT2_A.OrnamentPointer++;
+    PT2_A.Envelope_Enabled = false;
+    PT2_A.Position_In_Sample = 0;
+    PT2_A.Position_In_Ornament = 0;
+    PT2_A.Addition_To_Noise = 0;
+    PT2_A.Glissade = 0;
+    PT2_A.Current_Ton_Sliding=0;
+    PT2_A.GlissType = 0;
+    PT2_A.Enabled = False;
+    PT2_A.Number_Of_Notes_To_Skip = 0;
+    PT2_A.Note_Skip_Counter = 0;
+    PT2_A.Volume = 15;
+    PT2_A.Ton = 0;
+    
+    PT2_B.OrnamentPointer = PT2_A.OrnamentPointer;
+    PT2_B.Loop_Ornament_Position = PT2_A.Loop_Ornament_Position;
+    PT2_B.Ornament_Length = PT2_A.Ornament_Length;
+    PT2_B.Envelope_Enabled = false;
+    PT2_B.Position_In_Sample = 0;
+    PT2_B.Position_In_Ornament = 0;
+    PT2_B.Addition_To_Noise = 0;
+    PT2_B.Glissade = 0;
+    PT2_B.Current_Ton_Sliding = 0;
+    PT2_B.GlissType = 0;
+    PT2_B.Enabled = false;
+    PT2_B.Number_Of_Notes_To_Skip = 0;
+    PT2_B.Note_Skip_Counter = 0;
+    PT2_B.Volume = 15;
+    PT2_B.Ton = 0;
+    
+    PT2_C.OrnamentPointer = PT2_A.OrnamentPointer;
+    PT2_C.Loop_Ornament_Position = PT2_A.Loop_Ornament_Position;
+    PT2_C.Ornament_Length = PT2_A.Ornament_Length;
+    PT2_C.Envelope_Enabled = false;
+    PT2_C.Position_In_Sample = 0;
+    PT2_C.Position_In_Ornament = 0;
+    PT2_C.Addition_To_Noise = 0;
+    PT2_C.Glissade = 0;
+    PT2_C.Current_Ton_Sliding = 0;
+    PT2_C.GlissType = 0;
+    PT2_C.Enabled = false;
+    PT2_C.Number_Of_Notes_To_Skip = 0;
+    PT2_C.Note_Skip_Counter = 0;
+    PT2_C.Volume = 15;
+    PT2_C.Ton = 0;
+    
+    ay_resetay(&info, 0);
+    
 }
 
 void PT2_GetInfo(AYSongInfo &info)
@@ -176,16 +274,128 @@ void PT2_GetInfo(AYSongInfo &info)
 
 void PT2_Play(AYSongInfo &info)
 {
+    
+}
+
+void PT2_PatternInterpreter(AYSongInfo &info, PT2_Channel_Parameters &chan)
+{
+    unsigned char *module = info.module;
+    PT2_File *header = (STC_File *)module;
+    bool quit, gliss;
+    quit = false;
+    gliss = false;
     do
     {
-        z80ex_step(info.z80ctx);
+        unsigned char val = module[chan.Address_In_Pattern];
+        if(val >= 0xe1)
+        {
+            chan.SamplePointer = PT2_SamplesPointers(val - 0xe0);
+            chan.Sample_Length = module[chan.SamplePointer];
+            chan.SamplePointer++;
+            chan.Loop_Sample_Position = module[chan.SamplePointer];
+            chan.SamplePointer++;
+        }
+        else if(val == 0xe0)
+        {
+            chan.Position_In_Sample = 0;
+            chan.Position_In_Ornament = 0;
+            chan.Current_Ton_Sliding = 0;
+            chan.GlissType = 0;
+            chan.Enabled = false;
+            quit = true;
+        }
+        else if(val >= 0x80 && val <= 0xdf)
+        {
+            chan.Position_In_Sample = 0;
+            chan.Position_In_Ornament = 0;
+            chan.Current_Ton_Sliding = 0;
+            if(gliss)
+            {
+                chan.Slide_To_Note = val - 0x80;
+                if(chan.GlissType == 1)
+                    chan.Note = chan.Slide_To_Note;
+            }
+            else
+            {
+                chan.Note = val - 0x80;
+                chan.GlissType = 0;
+            }
+            chan.Enabled = true;
+            quit = true;
+        }
+        else if(val == 0x7f)
+            chan.Envelope_Enabled = false;
+        else if(val >= 0x71 && val <= 0x7e)
+        {
+            chan.Envelope_Enabled = true;
+            ay_writeay(&info, AY_ENV_SHAPE, val - 0x70);
+            chan.Address_In_Pattern++;
+            ay_writeay(&info, AY_ENV_FINE, module[Address_In_Pattern]);
+            chan.Address_In_Pattern++;
+            ay_writeay(&info, AY_ENV_COARSE, module[Address_In_Pattern]);
+        }
+        else if(val == 0x70)
+            quit = true;
+        else if(val >= 0x60 && val <= 0x6f)
+        {
+            chan.OrnamentPointer = PT2_OrnamentsPointers[val - 0x60];
+            chan.Ornament_Length = module[chan.OrnamentPointer];
+            chan.OrnamentPointer++;
+            chan.Loop_Ornament_Position = module[chan.OrnamentPointer];
+            chan.OrnamentPointer++;
+            chan.Position_In_Ornament = 0;
+        }
+        else if(val >= 0x20 && val <= 0x5f)
+            chan.Number_Of_Notes_To_Skip = val - 0x20;
+        else if(val >= 0x10 && val <= 0x1f)
+            Volume = val - 0x10;
+        else if(val == 0xf)
+        {
+            chan.Address_In_Pattern++;
+            PT2_Delay = module[Address_In_Pattern];
+        }
+        else if(val == 0xe)
+        {
+            chan.Address_In_Pattern++;
+            chan.Glissade = module[chan.Address_In_Pattern];
+            chan.GlissType = 1;
+            gliss = true;
+        }
+        else if(val == 0xd)
+        {
+            chan.Address_In_Pattern++;
+            chan.Glissade = abs((signed char)(module[Address_In_Pattern]));
+            chan.Address_In_Pattern += 2; //Not use precalculated Ton_Delta
+            //to avoide error with first note of pattern
+            chan.GlissType = 2;
+            gliss = true;
+        }
+        else if(val == 0xc)
+            chan.GlissType = 0;
+        else
+        {
+            chan.Address_In_Pattern++;
+            chan.Addition_To_Noise = module[chan.Address_In_Pattern];
+        }
+        chan.Address_In_Pattern++;
     }
-    while(z80ex_get_reg(info.z80ctx, regPC) != 8);
+    while(!quit);
+    if(gliss && (chan.GlissType == 2))
+    {
+        chan.Ton_Delta = abs(PT2_Table[chan.Slide_To_Note] - PT2_Table[chan.Note]);
+        if(chan.Slide_To_Note > chan.Note)
+            chan.Glissade = -chan.Glissade;
+    }
+    chan.Note_Skip_Counter = chan.Number_Of_Notes_To_Skip;
 }
 
 void PT2_Cleanup(AYSongInfo &info)
 {
-    ay_sys_shutdownz80(info);
+    if(info.data)
+    {
+        delete (PT2_SongInfo *)info.data;
+        info.data = 0;
+    }
 }
 
 bool PT2_Detect0(unsigned char *module, unsigned long length)
@@ -247,13 +457,13 @@ bool PT2_Detect1(unsigned char *module, unsigned long length)
         return false;
     if(PT2_SamplesPointers(0) != 0)
         return false;
-    
+
     j3 = PT2_SamplesPointers(0);
     if(PT2_OrnamentsPointers(0) - j3 - 2 > length)
         return false;
     if(PT2_OrnamentsPointers(0)- j3 < 0)
         return false;
-    
+
     j = 0;
     memcpy(&j, &module[PT2_OrnamentsPointers(0)], 3);
     if(j != 1)
@@ -289,18 +499,18 @@ bool PT2_Detect1(unsigned char *module, unsigned long length)
     header->PT2_NumberOfPositions = j2;
     for(j = 0; j < 32; j++)
     {
-        ay_sys_writeword(&header->PT2_SamplesPointers0 [j * 2], PT2_SamplesPointers(j) - j3);
+        ay_sys_writeword(&header->PT2_SamplesPointers0[j * 2], PT2_SamplesPointers(j) - j3);
     }
     for(j = 0; j < 16; j++)
     {
-        ay_sys_writeword(&header->PT2_OrnamentsPointers0 [j * 2], PT2_OrnamentsPointers(j) - j3);
+        ay_sys_writeword(&header->PT2_OrnamentsPointers0[j * 2], PT2_OrnamentsPointers(j) - j3);
     }
     for(j2 = 0; j2 < j1 * 3 + 1; j2++)
-     {
-      j = ay_sys_getword(&module[PT2_PatternsPointer+j2*2]);
-      j -= j3;
-      ay_sys_writeword(&module[PT2_PatternsPointer+j2*2], j);
-     }
+    {
+        j = ay_sys_getword(&module[PT2_PatternsPointer+j2 * 2]);
+        j -= j3;
+        ay_sys_writeword(&module[PT2_PatternsPointer+j2 * 2], j);
+    }
     return true;
 }
 
@@ -310,5 +520,5 @@ bool PT2_Detect(unsigned char *module, unsigned long length)
         if(!PT2_Detect1(module, length))
             return false;
     return true;
-        
+
 }
