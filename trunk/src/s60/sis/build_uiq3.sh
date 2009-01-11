e73b0de044f4713b86a620e800fbadd3089ac179
andrew@it-optima.ru <andrew@it-optima.ru@0fa6c16a-c44e-0410-b361-b1c6067f254c>
2009-01-11 16:46:00 +0000

    version 0.0.24!

--- trunk/src/s60/sis/build_uiq3.sh
+++ trunk/src/s60/sis/build_uiq3.sh
@@ -5,5 +5,5 @@ bldmake bldfiles
 abld build gcce urel
 cd ../sis
 makesis ayfly_uiq3.pkg
-signsis ayfly_uiq3.sis ayfly-uiq3-0.0.23.sisx gensign.cer gensign.key
+signsis ayfly_uiq3.sis ayfly-uiq3-0.0.24.sisx gensign.cer gensign.key
 rm ayfly_uiq3.sis
