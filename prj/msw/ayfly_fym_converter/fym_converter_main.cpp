5d963e90898bfba001bdb96e5c0188f17343c476
andrew <andrew@it-optima.ru>
2009-01-18 21:47:55 +0000

    ayfly_fym_converter gui version!

--- prj/msw/ayfly_fym_converter/fym_converter_main.cpp
+++ prj/msw/ayfly_fym_converter/fym_converter_main.cpp
@@ -23,11 +23,28 @@
 #include <fcntl.h>
 #include <vector>
 #include <zlib.h>
+#include <windows.h>
+#include <commctrl.h>
+#include <shlobj.h>
 
 bool end;
 char *out_dir;
 FILE *fxml;
 bool is_reg13;
+
+HWND editsrc;
+HWND editdst;
+HWND msglist;
+HWND convertbtn;
+#define ID_EDITSRC 1000
+#define ID_EDITDST 1001
+#define ID_MSGLIST 1002
+#define ID_GETSRC 1003
+#define ID_GETDST 1004
+#define ID_CONVERT 1005
+
+char srcdir [MAX_PATH * 2];
+char dstdir [MAX_PATH * 2];
 
 bool elapsed_callback(void *)
 {
@@ -165,7 +182,9 @@ void ProcessDir(char *dir, char *short_dir)
 					song = ay_initsong(fullpath, 44100, 0);
 					if(song)
 					{
-						printf("%s\n", res_path);
+						SendMessage(msglist, LB_ADDSTRING, 0, (LPARAM)dt.cFileName);
+						SendMessage(msglist, LB_SETCURSEL, SendMessage(msglist, LB_GETCOUNT, 0, 0) - 1, 0);
+						//printf("%s\n", res_path);
 						for(unsigned char i = 0; i < 14; i++)
 						{
 							regs [i].clear();
@@ -248,24 +267,17 @@ void ProcessDir(char *dir, char *short_dir)
 	}
 }
 
-int main(int argc, char **argv)
+void convert(char *srcdir, char *dstdir)
 {
-	fprintf(stderr, "AY 891x file to fym converter v. " AYFLY_VERSION_TEXT ", ");
-	fprintf(stderr, "Deryabin Andrew, 2009. GNU GPL v2 license.\n");
-	if(argc < 3)
-	{
-		usage();
-		exit(1);
-	}
-
-	out_dir = argv [2];
+	
+	out_dir = dstdir;
 
 	if(!CreateDirectory(out_dir, 0))
 	{
 		if(GetLastError() != ERROR_ALREADY_EXISTS)
 		{
 			fprintf(stderr, "Can't create output directory!!\n");
-			exit(1);
+			return;
 		}
 	}
 	
@@ -278,10 +290,191 @@ int main(int argc, char **argv)
 	str = "<fyms>\r\n";
 	fwrite(str, 1, strlen(str), fxml);
 
-	ProcessDir(argv [1], "");
+	ProcessDir(srcdir, "");
 
 	str = "</fyms>\r\n";
 	fwrite(str, 1, strlen(str), fxml);
+}
+
+DWORD WINAPI ThreadProc(LPVOID lpParameter)
+{
+	convert(srcdir, dstdir);
+	SendMessage(msglist, LB_ADDSTRING, 0, (LPARAM)"-->Conversion ended...");
+	SendMessage(msglist, LB_SETCURSEL, SendMessage(msglist, LB_GETCOUNT, 0, 0) - 1, 0);
+	EnableWindow(convertbtn, TRUE);
 
 	return 0;
+}
+
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch(uMsg)
+	{
+	case WM_CREATE:
+		{
+			CreateWindow("STATIC",
+						 "Source Path:",
+						 WS_CHILD | WS_VISIBLE,
+						 5, 7, 90, 20,
+						 hwnd, 
+						 0, 
+						 (HINSTANCE) GetWindowLong(hwnd, GWL_HINSTANCE), 
+                          NULL);       // pointer not needed 
+
+			editsrc = CreateWindow("EDIT",      // predefined class 
+                                    NULL,        // no window title 
+                                    WS_CHILD | WS_VISIBLE | ES_LEFT | WS_BORDER, 
+                                    100, 5, 260, 20,  // set size in WM_SIZE message 
+                                    hwnd,        // parent window 
+                                    (HMENU) ID_EDITSRC,   // edit control ID 
+                                    (HINSTANCE) GetWindowLong(hwnd, GWL_HINSTANCE), 
+                                    NULL);       // pointer not needed 
+
+			CreateWindow("BUTTON",
+						 "...",
+						 WS_CHILD | WS_VISIBLE,
+						 365, 5, 25, 20,
+						 hwnd, 
+						 (HMENU) ID_GETSRC, 
+						 (HINSTANCE) GetWindowLong(hwnd, GWL_HINSTANCE), 
+                          NULL);       // pointer not needed 
+
+			CreateWindow("STATIC",
+						 "Dst Path:",
+						 WS_CHILD | WS_VISIBLE,
+						 5, 37, 90, 20,
+						 hwnd, 
+						 0, 
+						 (HINSTANCE) GetWindowLong(hwnd, GWL_HINSTANCE), 
+                          NULL);       // pointer not needed 
+
+			editdst = CreateWindow("EDIT",      // predefined class 
+                                    NULL,        // no window title 
+                                    WS_CHILD | WS_VISIBLE | ES_LEFT | WS_BORDER, 
+                                    100, 35, 260, 20,  // set size in WM_SIZE message 
+                                    hwnd,        // parent window 
+                                    (HMENU) ID_EDITDST,   // edit control ID 
+                                    (HINSTANCE) GetWindowLong(hwnd, GWL_HINSTANCE), 
+                                    NULL);       // pointer not needed 
+
+			CreateWindow("BUTTON",
+						 "...",
+						 WS_CHILD | WS_VISIBLE,
+						 365, 35, 25, 20,
+						 hwnd, 
+						 (HMENU) ID_GETDST, 
+						 (HINSTANCE) GetWindowLong(hwnd, GWL_HINSTANCE), 
+                          NULL);       // pointer not needed 
+
+			msglist = CreateWindow("LISTBOX",      // predefined class 
+                                    NULL,        // no window title 
+									WS_CHILD | WS_VISIBLE | WS_BORDER | WS_VSCROLL, 
+                                    5, 65, 385, 160,  // set size in WM_SIZE message 
+                                    hwnd,        // parent window 
+                                    (HMENU) ID_MSGLIST,   // edit control ID 
+                                    (HINSTANCE) GetWindowLong(hwnd, GWL_HINSTANCE), 
+                                    NULL);       // pointer not needed
+
+			convertbtn = CreateWindow("BUTTON",
+						 "Convert !",
+						 WS_CHILD | WS_VISIBLE,
+						 320, 220, 70, 20,
+						 hwnd, 
+						 (HMENU) ID_CONVERT, 
+						 (HINSTANCE) GetWindowLong(hwnd, GWL_HINSTANCE), 
+                          NULL);       // pointer not needed
+
+		}
+		return 0;
+	case WM_DESTROY:
+		exit(0);
+		break;
+	case WM_CLOSE:
+		DestroyWindow(hwnd);
+		break;
+	case WM_COMMAND:
+		switch(HIWORD(wParam))
+		{
+		case BN_CLICKED:
+			if(LOWORD(wParam) == ID_GETSRC || LOWORD(wParam) == ID_GETDST)
+			{
+				BROWSEINFO info;
+				memset(&info, 0, sizeof(info));
+				info.hwndOwner = hwnd;
+				info.iImage = 0;
+				info.pidlRoot = 0;
+				info.lpszTitle = "Select folder...";
+				info.ulFlags = BIF_NEWDIALOGSTYLE;
+				info.lpfn = 0;
+				info.lParam = 0;
+				PIDLIST_ABSOLUTE pidl = SHBrowseForFolder(&info);
+				if(pidl)
+				{
+					char buffer [MAX_PATH * 2];
+					SHGetPathFromIDList(pidl, buffer);
+					if(LOWORD(wParam) == ID_GETSRC)
+						SetWindowText(editsrc, buffer);
+					else
+						SetWindowText(editdst, buffer);
+				}
+				
+			}
+			else if(LOWORD(wParam) == ID_CONVERT)
+			{				
+				GetWindowText(editsrc, srcdir, sizeof(srcdir));
+				GetWindowText(editdst, dstdir, sizeof(dstdir));
+				SendMessage(msglist, LB_RESETCONTENT, 0, 0);
+				SendMessage(msglist, LB_ADDSTRING, 0, (LPARAM)"-->Starting conversion...");
+				EnableWindow(convertbtn, FALSE);
+				CreateThread(0, 0, ThreadProc, 0, 0, 0);
+			}
+			return 0;
+			break;
+		default:
+			break;
+		}
+	default:
+		break;
+	}
+	return DefWindowProc(hwnd, uMsg, wParam, lParam);
+}
+
+
+int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
+{
+	CoInitialize(0);
+	INITCOMMONCONTROLSEX ccex;
+	ccex.dwSize = sizeof(ccex);
+	ccex.dwICC = ICC_STANDARD_CLASSES;
+	InitCommonControlsEx(&ccex);
+	ATOM a;
+	WNDCLASSEX wcex;
+	memset(&wcex, 0, sizeof(wcex));
+	wcex.cbSize = sizeof(wcex);
+	wcex.hbrBackground = (HBRUSH)COLOR_WINDOW;
+	wcex.hCursor = LoadCursor(0, IDC_ARROW);
+	wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(101));
+	wcex.hIconSm = wcex.hIcon;
+	wcex.hInstance = hInstance;
+	wcex.lpfnWndProc = WndProc;
+	wcex.lpszClassName = "Ayfly class";
+	wcex.style = CS_HREDRAW | CS_VREDRAW;
+	a = RegisterClassEx(&wcex);
+	HWND hWnd = CreateWindow((LPCSTR)a, "Ayfly to FYM converter v." AYFLY_VERSION_TEXT, 
+							 WS_CAPTION | WS_CLIPCHILDREN | WS_CLIPSIBLINGS | WS_SYSMENU, 
+							 CW_USEDEFAULT, CW_USEDEFAULT, 400, 270, 0, 0, 
+							 hInstance, 0);
+	ShowWindow(hWnd, SW_SHOW);
+	UpdateWindow(hWnd);
+	
+	MSG msg;
+	while(GetMessage(&msg, hWnd, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+	return 0;
+
+	
 }
